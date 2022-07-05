aefd3ae841e7f2e565349f5fe3ae92f55ccc24b8
brian432 <brian_cabrera123@outlook.es>
2022-07-05 12:35:19 -0300

    sekelton and render tasks added

--- src/componentes/Views/Tasks/Tasks.jsx
+++ src/componentes/Views/Tasks/Tasks.jsx
@@ -2,6 +2,7 @@ import { Header } from "../../Header/Header"
 import { TaskForm } from "../../TaskForm/TaskForm"
 import './Tasks.css'
 import { Card } from "../../Card/Card"
+import { Radio, RadioGroup, FormControlLabel, FormControl } from '@mui/material';
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import { useEffect, useState } from "react"
@@ -12,10 +13,11 @@ export const Tasks = () => {
 
     const [list, setList] = useState(null)
     const [loading, setLoading] = useState(false)
-
+    const [renderList, setRenderList] = useState(null)
+    const [tasksFromWho, setTasksFromWho] = useState("ALL")
     useEffect(() => {
         setLoading(true)
-        fetch(`${REACT_APP_API_ENDPOINT}task`, {
+        fetch(`${REACT_APP_API_ENDPOINT}task${tasksFromWho === "ME" ? "/me": ""}`, {
             headers: {
                 "Content-Type": "application/json",
                 Authorization: "Bearer " + localStorage.getItem("token")
@@ -24,33 +26,42 @@ export const Tasks = () => {
         })
             .then((response) => response.json())
             .then(data => {
+                setRenderList(data.result)
                 setList(data.result)
-                setTimeout(()=>{
+                setTimeout(() => {
                     setLoading(false) //agregamos esta funcion para retrasar la carga de data.result y asi poder ver el efecto del reloader <skeleton />
-                },10000)
+                }, 1000)
             })
-    }, [])
+    }, [tasksFromWho])
 
     const renderAllCards = () => {
         return (
-            list?.map(card => <Card key={card._id} card={card} />)
+            renderList?.map(card => <Card key={card._id} card={card} />)
         )
     }
 
     const renderNewCards = () => {
         return (
-            list?.filter(data => data.status === "NEW").map(card => <Card key={card._id} card={card} />)
+            renderList?.filter(data => data.status === "NEW").map(card => <Card key={card._id} card={card} />)
         )
     }
     const renderInProgressCards = () => {
         return (
-            list?.filter(data => data.status === "IN PROGRESS").map(card => <Card key={card._id} card={card} />)
+            renderList?.filter(data => data.status === "IN PROGRESS").map(card => <Card key={card._id} card={card} />)
         )
     }
     const renderFinishedCards = () => {
         return (
-            list?.filter(data => data.status === "FINISHED").map(card => <Card key={card._id} card={card} />)
+            renderList?.filter(data => data.status === "FINISHED").map(card => <Card key={card._id} card={card} />)
         )
+    }
+
+    const handleChangeimportance = (event) => {
+        if (event.currentTarget.value === "ALL") {
+            setRenderList(list)
+        } else {
+            setRenderList(list.filter(data => data.importance === event.currentTarget.value))
+        }
     }
 
     return (
@@ -62,34 +73,59 @@ export const Tasks = () => {
                     <div className="list_header">
                         <h2>Mis tareas</h2>
                     </div>
+                    <FormControl>
+                        <RadioGroup
+                            row
+                            aria-labelledby="demo-row-radio-buttons-group-label"
+                            onChange={(event)=> setTasksFromWho(event.currentTarget.value)}
+                        >
+                            <FormControlLabel
+                                value="ME"
+                                control={<Radio />}
+                                label="Mis tareas"
+                            />
+                            <FormControlLabel
+                                value="ALL"
+                                control={<Radio />}
+                                label="Todas"
+                            />
+                        </RadioGroup>
+                    </FormControl>
+                    <select name='importance' onChange={handleChangeimportance}>
+                        <option value="">Seleccionar opción</option>
+                        <option value="ALL">Todas</option>
+                        <option value="LOW">Baja</option>
+                        <option value="MEDIUM">Media</option>
+                        <option value="HIGH">Alta</option>
+                    </select>
                     <div className="list-group">
                         {
-                            !list?.length?
+                            !renderList?.length ?
                                 <div>No hay tareas creadas</div> :
-                                loading ? 
-                                <>
-                                    <Skeleton height={50} width={90} />
-                                </>:
-                                <>
-                                    <div className="list">
-                                        <h4>Nuevas</h4>
-                                        {
-                                            renderNewCards()
-                                        }
-                                    </div>
-                                    <div className="list">
-                                        <h4>En Progreso</h4>
-                                        {
-                                            renderInProgressCards()
-                                        }
-                                    </div>
-                                    <div className="list">
-                                        <h4>Finalizadas</h4>
-                                        {
-                                            renderFinishedCards()
-                                        }
-                                    </div>
-                                </>
+                                loading ?
+                                    <>
+                                        <Skeleton height={50} width={90} />
+                                    </> :
+                                    <>
+                                        <div className="list">
+                                            <h4>Nuevas</h4>
+                                            {
+                                                renderNewCards()
+                                            }
+                                        </div>
+                                        <div className="list">
+                                            <h4>En Progreso</h4>
+                                            {
+                                                renderInProgressCards()
+                                            }
+                                        </div>
+                                        <div className="list">
+                                            <h4>Finalizadas</h4>
+                                            {
+                                                renderFinishedCards()
+                                            }
+                                        </div>
+                                    </>
                         }
 
                     </div>
