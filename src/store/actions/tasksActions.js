ff593de0fedae351876d80cf58fb90f4f8b3581d
brian432 <brian_cabrera123@outlook.es>
2022-07-08 15:18:18 -0300

    reducers and actions added

--- src/store/actions/tasksActions.js
+++ src/store/actions/tasksActions.js
@@ -1,17 +1,25 @@
 const { REACT_APP_API_ENDPOINT } = process.env
 
-export const tasksRequest = () =>({
+export const tasksRequest = () => ({
     type: "TASKS_REQUEST"
 })
+export const tasksSucces = (data) => ({
+    type: "TASKS_SUCCESS",
+    payload: data
+})
+export const tasksFailure = (error) => ({
+    type: "TASKS_FAILURE",
+    payload: error
+})
 
-export const getTasks = (path) => dispatch => {
+export const getTasks = (path) => dispatch => {  
     dispatch(tasksRequest())
     fetch(`${REACT_APP_API_ENDPOINT}/task${path}`, {
-        headers: { 
-            'Content-type': 'applicaction/json', 
+        headers: {
+            'Content-type': 'applicaction/json',
             Autorization: "Bearer " + localStorage.getItem("token")
         }
-    }).then(response=>response.json())
-    .then(data=>{})
-    .catch(error=>{})
+    }).then(response => response.json())
+        .then(data => { dispatch(tasksSucces(data.result)) })
+        .catch(error => { dispatch(tasksFailure(error)) })
 }
