e2c99dbc3afb0d03b36e630bdf8132ef7d530ad7
Brian Cabrera <80001602+brian432@users.noreply.github.com>
2022-07-09 17:32:43 -0300

    Redux (#12)
    * Creation of reducer function creation of the store
    
    * tasksActions created
    
    * reducers and actions added
    
    * useDispatch and useSelector added
    
    * fix registered
    
    * error tasksReducer fix
    
    * taskAction added

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,7 +1,13 @@
 import './Header.css'
 import { useNavigate } from 'react-router-dom';
+
+import { useSelector } from "react-redux"
+
 export const Header = () => {
     const navigate = useNavigate()
+    const { tasks } = useSelector(state => { // traemos el estado del store mediante useSelector() y desestructutamos el estado con sus propiedades
+        return state.tasksReducer
+    })
 
     const HandleLogout = () => {
         localStorage.removeItem("token")
@@ -12,6 +18,7 @@ export const Header = () => {
         <header>
             <span>Go Scrum</span>
             <div className='wrapper_rigth_header'>
+                <div>Tareas creadas: {tasks?.length}</div>
                 <div>{localStorage.getItem("userName")}</div> {/*Cuando iniciamos sesion en el componente login, creamos un item en localStorage de la siguiente maner localStorage.setItem("userName", variableQueAlmacenaUserName)*/}
                 <div onClick={HandleLogout}>X</div>
             </div>
