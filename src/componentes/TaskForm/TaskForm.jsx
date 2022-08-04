02f4a48e5b36ae2668ac94930c0486aa7fd7b822
brian432 <brian_cabrera123@outlook.es>
2022-08-04 15:53:13 -0300

    taskForm refactor

--- src/componentes/TaskForm/TaskForm.jsx
+++ src/componentes/TaskForm/TaskForm.jsx
@@ -1,5 +1,5 @@
 import './TaskForm.css'
-import { useFormik } from "formik"
+import { Formik, Form, Field } from "formik"
 import * as Yup from 'yup'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,28 +9,15 @@ import { postTask, switchTaskCreated } from '../../store/actions/taskFormActions
 import { useEffect } from 'react';
 
 export const TaskForm = () => {
-
     const dispatch = useDispatch()
-    const  {taskCreated}  = useSelector(state => { return state.taskFormReducer })
+    const { taskCreated } = useSelector(state => { return state.taskFormReducer })
 
     useEffect(() => {
         if (taskCreated) {
-            resetForm()
             toast("Tu tarea se creo")
             dispatch(switchTaskCreated())
         }
     }, [taskCreated])
-
-    const initialValues = {
-        title: "",
-        status: "",
-        importance: "",
-        description: ""
-    }
-
-    const onSubmit = () => {
-        dispatch(postTask(values))
-    }
 
     const required = "* Campo obligatorio"
 
@@ -40,52 +27,78 @@ export const TaskForm = () => {
         importance: Yup.string().required(required),
         description: Yup.string().required(required),
     })
-
-    const formik = useFormik({ initialValues, validationSchema, onSubmit })
-    const { handleChange, handleSubmit, errors, touched, handleBlur, values, resetForm } = formik 
-
-
     return (
         <section className="task-form">
             <h2>Crear tarea</h2>
             <p>Crea tus tareas</p>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <div>
-                        <input name="title" value={values.title} onChange={handleChange} onBlur={handleBlur} placeholder="Título" />
-                        {errors.title && touched.title && <span className='primaryColor'>{errors.title}</span>}
-                    </div>
-                    <div>
-                        <select name="status" value={values.status} onChange={handleChange} onBlur={handleBlur} >
-                            <option value="">Seleccionar opción</option>
-                            <option value="NEW">Nuevo</option>
-                            <option value="IN PROGRESS">En proceso</option>
-                            <option value="FINISHED">Terminada</option>
-                        </select>
-                        {errors.status && touched.status && <span className='primaryColor'>{errors.status}</span>}
-                    </div>
-                    <div>
-                        <select name='importance' value={values.importance} onChange={handleChange} onBlur={handleBlur}>
-                            <option value="">Seleccionar opción</option>
-                            <option value="LOW">Baja</option>
-                            <option value="MEDIUM">Media</option>
-                            <option value="HIGH">Alta</option>
-                        </select>
-                        {errors.importance && touched.importance && <span className='primaryColor'>{errors.importance}</span>}
-                    </div>
-                    <div>
-                        <textarea
-                            name="description"
-                            value={values.description}
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            placeholder="Descripción"
-                        />
-                        {errors.description && touched.description && <span className='primaryColor'>{errors.description}</span>}
-                    </div>
-                    <button type='submit'>Crear</button>
-                </div>
-            </form>
+            <Formik
+                initialValues={{
+                    title: "",
+                    status: "",
+                    importance: "",
+                    description: ""
+                }}
+                validationSchema={validationSchema}
+                onSubmit={(values, { resetForm }) => {
+                    dispatch(postTask(values))
+                    resetForm()
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form>
+                        <div>
+                            <div>
+                                <Field
+                                    name="title"
+                                    placeholder="Título"
+                                />
+                                {errors.title && touched.title  ? (
+                                    <span className='primaryColor'>{errors.title}</span>
+                                ) : null}
+                            </div>
+                            <div>
+                                <Field
+                                    as="select"
+                                    name="status"
+                                >
+                                    <option value="">Seleccionar opción</option>
+                                    <option value="NEW">Nuevo</option>
+                                    <option value="IN PROGRESS">En proceso</option>
+                                    <option value="FINISHED">Terminada</option>
+                                </Field>
+                                {errors.status && touched.status ? (
+                                    <span className='primaryColor'>{errors.status}</span>
+                                ) : null}
+                            </div>
+                            <div>
+                                <Field
+                                    as="select"
+                                    name="importance"
+                                >
+                                    <option value="">Seleccionar opción</option>
+                                    <option value="LOW">Baja</option>
+                                    <option value="MEDIUM">Media</option>
+                                    <option value="HIGH">Alta</option>
+                                </Field>
+                                {errors.importance && touched.importance ? (
+                                    <span className='primaryColor'>{errors.importance}</span>
+                                ) : null}
+                            </div>
+                            <div>
+                                <Field
+                                    as="textarea"
+                                    name="description"
+                                    placeholder="Descripción"
+                                />
+                                {errors.description && touched.description  ? (
+                                    <span className='primaryColor'>{errors.description}</span>
+                                ) : null}
+                            </div>
+                            <button type='submit'>Crear</button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
             <ToastContainer />
         </section>
     )
